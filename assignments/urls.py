f26a8c9121808f6add31019030ca40c67f23a839
Charles Magahern <charles@magahern.com>
2012-07-05 20:52:38 -0700

    Fixed bug with calendar html url

--- assignments/urls.py
+++ assignments/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$', views.assignments, name='assignments'),
-	url(r'^(?P<assignment_id>.+)', views.view_assignment, name='view_assignment'),
+	url(r'^(?P<assignment_id>\d+)', views.view_assignment, name='view_assignment'),
 	url(r'^calendar\.html', views.calendar, name="assignments_calendar")
 )
