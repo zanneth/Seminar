b2ffd9987411b200a881e305365bc77288717738
Charles Magahern <charles@magahern.com>
2012-08-01 21:23:17 -0700

    Basic materials view implemented.

--- materials/views.py
+++ materials/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from core.decorators import selected_course_required
+from materials import models
+import core.models
 
 @login_required
 @selected_course_required
 def materials(request):
+	course = core.models.Course.get_selected_course(request)
+	groups = course.material_groups.all()
 	return render_to_response("materials.html",
+							{ "material_groups" : groups },
 							context_instance=RequestContext(request))
